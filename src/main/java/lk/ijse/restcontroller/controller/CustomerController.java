977a1f0a3b31f049e0a226ddbc5940bb2f1b901d
BinaraDulsara <binaradulsara021@gmail.com>
2023-10-12 05:21:46 +0530

    when we're using two @GetMapping in the same controller

--- src/main/java/lk/ijse/restcontroller/controller/CustomerController.java
+++ src/main/java/lk/ijse/restcontroller/controller/CustomerController.java
@@ -1,25 +1,43 @@
 package lk.ijse.restcontroller.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //REST = Reprasantational state transfer (Architecture ek)
 @RestController
-@RequestMapping("customer")
+@RequestMapping("customer")//Handler Mapping
+
+
 public class CustomerController {
 
-
-
-    @GetMapping
-    public String getAllCustomer(){
-        return " Hello there !!! ";
+    @GetMapping //Handler Mapping
+    public String getAllCustomer(){ //Handler Methods
+        return "Get all Customer and Get method invoked";
     }
 
-    @PostMapping
-    public String saveCustomer(){
-        return "Customer save and post method invoke";
+    @GetMapping(path="search") //Handler Mapping
+    public String SearchCustomer(){ //Handler Methods
+        return "Search Customer and Get method invoked";
+    }
+
+    @GetMapping(path="newId") //Handler Mapping
+    public String genarateNewId(){ //Handler Methods
+        return "new Customer id and Get method invoked";
+    }
+
+
+    @PostMapping//Handler Mapping
+    public String saveCustomer(){ //Handler Methods
+        return "Customer save and post method invoked";
+    }
+
+    @DeleteMapping//Handler Mapping
+    public String deleteCustomer(){ //Handler Methods
+        return "Customer deleted and delete method invoked";
+    }
+
+    @PutMapping//Handler Mapping
+    public String updateCustomer(){ //Handler Methods
+        return "Customer updated nd Put method invoked";
     }
 
 }
