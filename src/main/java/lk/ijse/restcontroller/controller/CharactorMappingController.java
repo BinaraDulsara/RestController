08d025fd83ef1388b7f49639a97d8acb1d64cf3f
BinaraDulsara <binaradulsara021@gmail.com>
2023-10-12 06:06:04 +0530

    CharactorMapping introduced

---
+++ src/main/java/lk/ijse/restcontroller/controller/CharactorMappingController.java
@@ -0,0 +1,28 @@
+package lk.ijse.restcontroller.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("character")
+public class CharactorMappingController {
+
+    //item/I333
+    //item/Ivff
+    //item/I001
+
+
+    @GetMapping(path = "iteam/i???")
+    public String test(){
+        return "Get Mapping Invoked (test) ";
+    }
+
+/*
+    @GetMapping(path = "")
+    public String test2(){
+        return "Get Mapping Invoked (test2)";
+    }
+*/
+
+}
--- src/main/java/lk/ijse/restcontroller/controller/ItemController.java
+++
@@ -1,4 +0,0 @@
-package lk.ijse.restcontroller.controller;
-
-public class ItemController {
-}
