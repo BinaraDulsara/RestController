0e30a68fb678ee3fc76d2fd1af739f293c820131
BinaraDulsara <binaradulsara021@gmail.com>
2023-10-12 05:57:14 +0530

    did Exact mapping

---
+++ src/main/java/lk/ijse/restcontroller/controller/ExactMappingController.java
@@ -0,0 +1,25 @@
+package lk.ijse.restcontroller.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("exact")
+public class ExactMappingController {
+
+    //Exact Mapping = .....item/it/test1
+    //Exact Mapping = .....item/test2/i0/tt1
+    //path segment  = --> /it/
+
+    @GetMapping(path = "it/test1")
+    public String test(){
+        return "Get Mapping Invoked (test) ";
+    }
+
+    @GetMapping(path = "test2/i0/tt1")
+    public String test2(){
+        return "Get Mapping Invoked (test2)";
+    }
+
+}
--- src/main/java/lk/ijse/restcontroller/controller/ItemController.java
+++ src/main/java/lk/ijse/restcontroller/controller/ItemController.java
@@ -1,15 +1,4 @@
 package lk.ijse.restcontroller.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("item")
 public class ItemController {
-
-    @GetMapping
-    public String saveItem(){
-        return "Item Saved and Get Mapping invoked";
-    }
 }
