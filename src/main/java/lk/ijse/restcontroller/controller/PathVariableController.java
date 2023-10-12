6786cdfab6bcd09c847cc54bd03234224ed44e12
BinaraDulsara <binaradulsara021@gmail.com>
2023-10-12 08:11:24 +0530

    WildCardMapping introduced mapping option two

---
+++ src/main/java/lk/ijse/restcontroller/controller/PathVariableController.java
@@ -0,0 +1,20 @@
+package lk.ijse.restcontroller.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("variable")
+public class PathVariableController {
+
+    //http://localhost:8080/restcontroller/bariable/I001
+
+    @GetMapping(path = "{I001}")
+    public String test(@PathVariable ("I001") String itemCode){
+        return itemCode;
+    }
+
+
+}
